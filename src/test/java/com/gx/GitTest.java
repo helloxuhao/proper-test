004c25065d9f3f5cff8e310b3bdd2b421bd23906
xuhao <xuhao@atguigu.com>
2022-01-11 08:11:32 +0800

    hon-fix commit

--- src/test/java/com/gx/GitTest.java
+++ src/test/java/com/gx/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String []args){
         System.out.println("fqwfqeq");
         System.out.println("test3");
+        System.out.println("hon-fix");
     }
 }
